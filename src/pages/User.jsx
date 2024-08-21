a2b28aa074df067cfbee133244a3383654666d24
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-08-21 20:20:02 +0530

    chore: add user search tab

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 // import { useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
 import {
@@ -13,6 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { IoMdArrowBack } from "react-icons/io";
 import { FaPlus } from "react-icons/fa6";
+import { SearchIcon } from "lucide-react";
 const data = [
   {
     user_id: "001",
@@ -57,22 +58,75 @@ const data = [
 ];
 
 export default function User() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortConfig, setSortConfig] = useState(null);
+
+  const filteredUsers = useMemo(() => {
+    return data.filter(
+      (data) =>
+        data.full_name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        data.user_id.includes(searchQuery)
+    );
+  }, [searchQuery, data]);
+
+  const sortedUsers = useMemo(() => {
+    if (sortConfig !== null) {
+      return [...filteredUsers].sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return filteredUsers;
+  }, [filteredUsers, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
   return (
     <div className="flex-1 h-screen mx-[32px] mt-[32px] ">
       <div className="flex justify-between items-center mb-[24px]">
-        <div>
+        <div className="">
           <h1 className="text-[#00263E] text-[24px] font-[700]">Users</h1>
           <p className="text-[#72849A]">Yano's user database</p>
         </div>
 
-        <Link to="createUser">
-          <button className=" flex gap-3 items-center justify-center text-[#fff] text-[16px] font-[600] bg-pink px-[10px] py-[8px] rounded-[4px]">
-            <span>
-              <FaPlus size={14} />
-            </span>
-            Create a new user
-          </button>
-        </Link>
+        <div className="flex justify-around items-center">
+          <form className="p-[16px] ">
+            <div className="flex items-center border rounded-[8px] bg-[#EEEEEE] h-[40px] px-2 ">
+              <SearchIcon className="  h-4 w-4 text-gray-500" />
+              <input
+                className="w-full bg-transparent shadow-none border-none outline-none pl-2 "
+                placeholder="Search..."
+                type="search"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </div>
+          </form>
+          <Link to="createUser">
+            <button className=" flex gap-3 items-center justify-center text-[#fff] text-[16px] font-[600] bg-pink px-[10px] py-[8px] rounded-[4px]">
+              <span>
+                <FaPlus size={14} />
+              </span>
+              Create a new user
+            </button>
+          </Link>
+        </div>
       </div>
 
       <div className="flex flex-col flex-1">
@@ -81,22 +135,55 @@ export default function User() {
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead>
+                  <TableHead
+                    onClick={() => requestSort("id")}
+                    className="cursor-pointer "
+                  >
                     <div className="flex items-center gap-2">
                       <input type="checkbox" />
                       User ID
                     </div>
                   </TableHead>
-                  <TableHead>Full Name</TableHead>
-                  <TableHead>Country</TableHead>
-                  <TableHead>Type</TableHead>
-                  <TableHead>Date of creation</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead>Actions</TableHead>
+                  <TableHead
+                    onClick={() => requestSort("name")}
+                    className="cursor-pointer "
+                  >
+                    Full Name
+                  </TableHead>
+                  <TableHead
+                    onClick={() => requestSort("country")}
+                    className="cursor-pointer "
+                  >
+                    Country
+                  </TableHead>
+                  <TableHead
+                    onClick={() => requestSort("type")}
+                    className="cursor-pointer "
+                  >
+                    Type
+                  </TableHead>
+                  <TableHead
+                    onClick={() => requestSort("date")}
+                    className="cursor-pointer "
+                  >
+                    Date of creation
+                  </TableHead>
+                  <TableHead
+                    onClick={() => requestSort("status")}
+                    className="cursor-pointer "
+                  >
+                    Status
+                  </TableHead>
+                  <TableHead
+                    onClick={() => requestSort("action")}
+                    className="cursor-pointer "
+                  >
+                    Actions
+                  </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {data?.map((user) => (
+                {sortedUsers?.map((user) => (
                   <TableRow key={user?.user_id}>
                     <TableCell>
                       <div className="flex items-center gap-2">
